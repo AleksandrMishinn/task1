82881ca3395e26c01b359a79a76765f8e95b6c1b
Владимир Рязанов <v.ryazanov@andersenlab.com>
2019-10-02 00:00:31 +0300

    Initial commit

---
+++ src/com/company/Test.java
@@ -0,0 +1,23 @@
+package com.company;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Необходимо уменьшить время выполнения вычислений.
+ * <p>
+ * Можно изменять этот класс или добавить новый. Решение должно быть максимально простым, без использования экзекуторов.
+ * (Runnable, Threads). Аккуратно обработайте потенциальные ошибки (разумеется вычисления если появились ошибки нужно
+ * остановить)
+ */
+public class Test {
+    public static void main(String[] args) throws TestException {
+        Set<Double> res = new HashSet<>();
+
+        for (int i = 0; i < TestConsts.N; i++) {
+            res.addAll(TestCalc.calculate(i));
+        }
+
+        System.out.println(res);
+    }
+}
