69a5416c6358547f5ded068afc7496d1aeaf4e96
Aleksandr <alexandrmishinn@gmail.com>
2020-09-02 14:47:31 +0300

    Уменьшено время выполнения вычислений.

--- src/com/company/Test.java
+++ src/com/company/Test.java
@@ -1,7 +1,7 @@
 package com.company;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
+import java.util.logging.Logger;
 
 /**
  * Необходимо уменьшить время выполнения вычислений.
@@ -11,13 +11,52 @@ import java.util.Set;
  * остановить). Количество потоков должно быть ограничено значением константы com.company.TestConsts#MAX_THREADS.
  */
 public class Test {
-    public static void main(String[] args) throws TestException {
-        Set<Double> res = new HashSet<>();
 
-        for (int i = 0; i < TestConsts.N; i++) {
-            res.addAll(TestCalc.calculate(i));
+    private static volatile boolean allTasksCompleted = false;
+    private static volatile Queue<Integer> taskQueue = new LinkedList<>();
+    final static Logger LOGGER = Logger.getLogger(Test.class.getName());
+
+    public static void main(String[] args) {
+
+        Set<Double> result = new HashSet<>();
+        List<Thread> threads = new ArrayList<>();
+
+        final Runnable executor = () -> {
+            while (!(Test.allTasksCompleted && taskQueue.isEmpty())) {
+                Integer currentNumber = taskQueue.poll();
+                if (currentNumber != null) {
+                    try {
+                        Set<Double> currentResult = TestCalc.calculate(currentNumber);
+                        result.addAll(currentResult);
+                    } catch (TestException e) {
+                        LOGGER.warning("Ошибка в вычислениях. Обрабатываемое число: " + currentNumber);
+                        e.printStackTrace();
+                        break;
+                    }
+                }
+            }
+        };
+
+        for (int i = 0; i < TestConsts.MAX_THREADS; i++) {
+            Thread currentThread = new Thread(executor);
+            threads.add(currentThread);
+            currentThread.start();
         }
 
-        System.out.println(res);
+        for (int i = 0; i < TestConsts.N; i++) {
+            taskQueue.add(i);
+        }
+
+        allTasksCompleted = true;
+
+        for (Thread currentThread : threads) {
+            try {
+                currentThread.join();
+            } catch (InterruptedException e) {
+                LOGGER.warning("Ошибка в одном при исполнении потока " + currentThread);
+                e.printStackTrace();
+            }
+        }
+        LOGGER.info(String.valueOf(result));
     }
 }
